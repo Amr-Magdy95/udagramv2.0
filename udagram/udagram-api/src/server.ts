4d7b96622ad8bf161ebca03723b5f2f6256bc1f1
Amr-Magdy95 <37376273+Amr-Magdy95@users.noreply.github.com>
2022-10-30 16:45:17 +0200

    added debugging step

--- udagram/udagram-api/src/server.ts
+++ udagram/udagram-api/src/server.ts
@@ -6,7 +6,7 @@ import { sequelize } from "./sequelize";
 import { IndexRouter } from "./controllers/v0/index.router";
 
 import bodyParser from "body-parser";
-import { V0_FEED_MODELS, V0_USER_MODELS } from "./controllers/v0/model.index";
+import { V0_FEED_MODELS, V0_USER_MODELS } from "./controllers/v0/model.index" ;
 
 (async () => {
   dotenv.config();
